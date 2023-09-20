40483486aa04836b5afe9fdd7177dfa28dd0505a
abdurahmonrahmatjonov <abdurahmonrahmatjonov89@gmail.com>
2023-09-20 09:48:48 +0500

    random color

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-const btnclilc =document.querySelector("btn")
+const btnclilck =document.querySelector("btn")
+ function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+btnclilck.addEventListener('click',() => {
+var randomColor = getRandomColor();
+ btnclilck.style.color  =randomColor
+})
